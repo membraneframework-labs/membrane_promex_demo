d78fa0e1ce102e7ed04ed648da74e45a68bd02c3
Krzysztof Wende <krzysztof.wende@swmansion.com>
2025-02-27 12:34:48 +0100

    Cleanup

--- lib/pipeline.ex
+++ lib/pipeline.ex
@@ -5,8 +5,6 @@ defmodule MembranePromexDemo.Pipeline do
   import Membrane.ChildrenSpec
   require Logger
 
-  import Telemetry.Metrics
-
   def start_link(state) do
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end
@@ -34,42 +32,6 @@ defmodule MembranePromexDemo.Pipeline do
       child(:source, %Testing.Source{output: ["a", "b", "c"]})
       |> child(:filter, TestFilter)
       |> child(:sink, Testing.Sink)
-
-    now = System.system_time(:second)
-    {uptime_ms, _} = :erlang.statistics(:wall_clock)
-    beam_start_time = now - div(uptime_ms, 1000)
-
-    Telemetry.Metrics.ConsoleReporter.start_link(
-      metrics:
-        for handler <- [:handle_buffer, :handle_setup, :handle_init] do
-          last_value([:membrane, :element, handler, :stop, :duration],
-            description: "Duration of membrejn #{handler} callback",
-            unit: {:native, :millisecond},
-            tags: [:spanID, :traceID, :serviceName, :operationName, :parentSpanID, :startTime],
-            tag_values: fn meta = %{callback_args: cb_args} ->
-              b =
-                case cb_args do
-                  [_, b, _, _] -> b
-                  [_, b] -> b
-                end
-
-              %{
-                spanID: inspect(meta.component_path),
-                traceID: inspect(beam_start_time),
-                serviceName: inspect(meta.callback_context.name),
-                operationName: inspect(handler),
-                parentSpanID: "",
-                startTime:
-                  :erlang.convert_time_unit(
-                    meta.monotonic_time + :erlang.time_offset(),
-                    :native,
-                    :millisecond
-                  )
-              }
-            end
-          )
-        end
-    )
 
     _pid = Testing.Pipeline.start(spec: child_spec)
     {:ok, stack}
