606b29302f99bd4957634d2ccdf124cbdcc6e4fe
Łukasz Kita <lukasz.kita0@gmail.com>
2025-03-12 12:12:22 +0100

    Move pipeline creation to the controller

--- lib/membrane_promex_demo_web/controllers/page_controller.ex
+++ lib/membrane_promex_demo_web/controllers/page_controller.ex
@@ -4,6 +4,7 @@ defmodule MembranePromexDemoWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
+    {:ok, _supervisor, _pid} = Membrane.Pipeline.start(MembranePromexDemo.Membrane.Pipeline)
     render(conn, :home, layout: false)
   end
 end
